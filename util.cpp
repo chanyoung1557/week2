ccd5422f006f3f67f1246c8e76bd3bda2a1c8012
chanyoung1557 <158740164+chanyoung1557@users.noreply.github.com>
2024-03-23 00:04:30 +0900

    fix: 정답의 숫자가 중복되지 않도록 변경

--- util.cpp
+++ util.cpp
@@ -3,5 +3,29 @@
 #include <ctime>
 int ran() {
     srand((unsigned int)time(NULL));
-    return rand()%1000;
+    int number;
+    bool digits[10] = {false};
+    do {
+        number = rand() % 1000;
+        int temp = number;
+        bool isUnique = true;
+        while (temp > 0) {
+            int digit = temp % 10;
+            if (digits[digit]) {
+                isUnique = false;
+                break;
+            }
+            digits[digit] = true;
+            temp /= 10;
+        }
+        if (isUnique) {
+            break;
+        } else {
+            for (int i = 0; i < 10; ++i) {
+                digits[i] = false;
+            }
+        }
+    } while (true);
+
+    return number;
 }
